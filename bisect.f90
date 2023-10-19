3627deb3288d25e1e1f32fbe9be3e1ef39f65428
melt <thomas.meltzer1@gmail.com>
2023-10-19 15:13:14 +0100

    added new model version 27

--- bisect.f90
+++ bisect.f90
@@ -45,7 +45,7 @@ program weathermodel
 
       call sleep(1)
 
-      print *, 'running model version 26'
+      print *, 'running model version 27'
 
       do i = 1, N
         answer(i) = i
@@ -302,5 +302,15 @@ program weathermodel
       print *, 'does important stuff'
 
     end subroutine newsub26
+
+
+    subroutine newsub27(var)
+      implicit none
+
+      integer, intent(inout) :: var
+
+      print *, 'does important stuff'
+
+    end subroutine newsub27
 
 end program weathermodel
